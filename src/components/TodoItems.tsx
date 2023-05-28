2d9d1f842d86afaf77a53acd06cbc6856398a936
naveensomanna26 <naveensomanna26@gmail.com>
2023-05-28 18:45:34 +0530

    next js with react server and client component

---
+++ src/components/TodoItems.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+type TodoItemsProps = {
+  id: string;
+  title: string;
+  complete: boolean;
+  toggleTodo: (id: string, complete: boolean) => void;
+};
+
+export function TodoItems({ id, title, complete, toggleTodo }: TodoItemsProps) {
+  return (
+    <li className="flex gap-1 items-center">
+      {" "}
+      <input
+        id={id}
+        type="checkbox"
+        className="cursor-pointer peer"
+        defaultChecked={complete}
+        onChange={(e) => toggleTodo(id, e.target.checked)}
+      />
+      <label
+        htmlFor={id}
+        className="cursor-pointer peer-checked:line-through peer-checked:text-slate-500"
+      >
+        {title}
+      </label>
+    </li>
+  );
+}
